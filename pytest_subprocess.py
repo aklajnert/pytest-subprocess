3ff7f22fff7cffd311c39dcfaab99d243fe3bcad
Andrzej Klajnert <github@aklajnert.pl>
2019-11-16 16:41:08 +0100

    Prepared basic scaffolding for the project.

--- pytest_subprocess.py
+++ pytest_subprocess.py
@@ -1,21 +1,37 @@
 # -*- coding: utf-8 -*-
+import typing
 
 import pytest
 
+process_list : typing.List['Process'] = []
 
-def pytest_addoption(parser):
-    group = parser.getgroup('subprocess')
-    group.addoption(
-        '--foo',
-        action='store',
-        dest='dest_foo',
-        default='2019',
-        help='Set the value for the fixture "bar".'
-    )
 
-    parser.addini('HELLO', 'Dummy pytest.ini setting')
+class FakePopen:
+    """The base class that fakes the real subprocess"""
+    def __init__(self, command: typing.Union[typing.Tuple[str], str]):
+        self.command: typing.Union[typing.List[str], typing.Tuple[str], str] = command
 
+
+class Process:
+    """Class responsible for tracking the processes"""
+
+    def __init__(self):
+        self.processes: typing.Dict[typing.Union[str, typing.Tuple[str]], FakePopen] = dict()
+
+    def register_subprocess(self, command: typing.Union[typing.List[str], typing.Tuple[str], str]):
+        if isinstance(command, list):
+            command = tuple(command)
+
+        self.processes[command] = FakePopen(command)
+
+    def __enter__(self) -> 'Process':
+        process_list.append(self)
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        process_list.remove(self)
 
 @pytest.fixture
-def bar(request):
-    return request.config.option.dest_foo
+def process() -> Process:
+    with Process():
+        yield
