3b9932888582f369f2a3ed3d76d55a9aad256e89
Andrzej Klajnert <github@aklajnert.pl>
2019-11-16 16:41:35 +0100

    Run autoformat

--- pytest_subprocess.py
+++ pytest_subprocess.py
@@ -3,11 +3,12 @@ import typing
 
 import pytest
 
-process_list : typing.List['Process'] = []
+process_list: typing.List["Process"] = []
 
 
 class FakePopen:
     """The base class that fakes the real subprocess"""
+
     def __init__(self, command: typing.Union[typing.Tuple[str], str]):
         self.command: typing.Union[typing.List[str], typing.Tuple[str], str] = command
 
@@ -16,20 +17,25 @@ class Process:
     """Class responsible for tracking the processes"""
 
     def __init__(self):
-        self.processes: typing.Dict[typing.Union[str, typing.Tuple[str]], FakePopen] = dict()
+        self.processes: typing.Dict[
+            typing.Union[str, typing.Tuple[str]], FakePopen
+        ] = dict()
 
-    def register_subprocess(self, command: typing.Union[typing.List[str], typing.Tuple[str], str]):
+    def register_subprocess(
+        self, command: typing.Union[typing.List[str], typing.Tuple[str], str]
+    ):
         if isinstance(command, list):
             command = tuple(command)
 
         self.processes[command] = FakePopen(command)
 
-    def __enter__(self) -> 'Process':
+    def __enter__(self) -> "Process":
         process_list.append(self)
         return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         process_list.remove(self)
+
 
 @pytest.fixture
 def process() -> Process:
--- tests/test_subprocess.py
+++ tests/test_subprocess.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
 
+
 def test_basic_process(process):
     assert process
