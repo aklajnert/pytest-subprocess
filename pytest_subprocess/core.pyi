b451561ddd4055c94387908ba74acc19a0b410cb
Andrzej Klajnert <github@aklajnert.pl>
2019-12-11 19:02:59 +0100

    Removed unused import

--- pytest_subprocess/core.pyi
+++ pytest_subprocess/core.pyi
@@ -3,8 +3,6 @@ import io
 import threading
 import typing
 
-import pytest  # type: ignore
-
 OPTIONAL_TEXT = typing.Union[str, bytes, None]
 OPTIONAL_TEXT_OR_ITERABLE = typing.Union[
     str,
@@ -15,9 +13,11 @@ OPTIONAL_TEXT_OR_ITERABLE = typing.Union[
 ]
 BUFFER = typing.Union[None, io.BytesIO, io.StringIO]
 
+
 def _ensure_hashable(
-    input: typing.Union[typing.List[str], typing.Tuple[str, ...], str]
+        input: typing.Union[typing.List[str], typing.Tuple[str, ...], str]
 ) -> typing.Union[typing.Tuple[str, ...], str]: ...
+
 
 class FakePopen:
     args: typing.Union[typing.List[str], typing.Tuple[str, ...], str]
@@ -34,35 +34,51 @@ class FakePopen:
     __callback: typing.Optional[typing.Optional[typing.Callable]]
     __stdin_callable: typing.Optional[typing.Optional[typing.Callable]]
     __thread: typing.Optional[threading.Thread]
+
     def __init__(
-        self,
-        command: typing.Union[typing.Tuple[str, ...], str],
-        stdout: OPTIONAL_TEXT_OR_ITERABLE = None,
-        stderr: OPTIONAL_TEXT_OR_ITERABLE = None,
-        returncode: int = 0,
-        wait: typing.Optional[float] = None,
-        callback: typing.Optional[typing.Callable] = None,
-        stdin_callable: typing.Optional[typing.Callable] = None,
-        **_: typing.Dict[str, typing.Any]
+            self,
+            command: typing.Union[typing.Tuple[str, ...], str],
+            stdout: OPTIONAL_TEXT_OR_ITERABLE = None,
+            stderr: OPTIONAL_TEXT_OR_ITERABLE = None,
+            returncode: int = 0,
+            wait: typing.Optional[float] = None,
+            callback: typing.Optional[typing.Callable] = None,
+            stdin_callable: typing.Optional[typing.Callable] = None,
+            **_: typing.Dict[str, typing.Any]
     ) -> None: ...
+
     def __enter__(self) -> "FakePopen": ...
+
     def __exit__(self, *args: typing.List, **kwargs: typing.Dict) -> None: ...
+
     def communicate(
-        self, input: OPTIONAL_TEXT = ..., timeout: typing.Optional[float] = ...,
+            self, input: OPTIONAL_TEXT = ..., timeout: typing.Optional[float] = ...,
     ) -> typing.Tuple[typing.Any, typing.Any]: ...
-    def _extend_stream_from_dict(self, dictionary: typing.Dict[str, typing.Any], key: str, stream: BUFFER) -> BUFFER: ...
+
+    def _extend_stream_from_dict(self, dictionary: typing.Dict[str, typing.Any], key: str,
+                                 stream: BUFFER) -> BUFFER: ...
+
     def poll(self) -> None: ...
+
     def wait(self, timeout: typing.Optional[float] = None) -> int: ...
+
     def configure(self, **kwargs: typing.Optional[typing.Dict]) -> None: ...
+
     def _prepare_buffer(
-        self, input: typing.Union[str, bytes, None], io_base: BUFFER = None,
+            self, input: typing.Union[str, bytes, None], io_base: BUFFER = None,
     ) -> io.BytesIO: ...
+
     def _convert(self, input: typing.Union[str, bytes]) -> typing.Union[str, bytes]: ...
+
     def _wait(self, wait_period: float) -> None: ...
+
     def run_thread(self) -> None: ...
+
     def _finish_process(self) -> None: ...
 
+
 class ProcessNotRegisteredError(Exception): ...
+
 
 class ProcessDispatcher:
     process_list: typing.List["FakeProcess"]
@@ -71,47 +87,64 @@ class ProcessDispatcher:
     _cache: typing.Dict[FakeProcess, typing.Dict[FakeProcess, typing.Any]]
     _keep_last_process: bool
     _pid: bool
+
     @classmethod
     def register(cls, process: "FakeProcess") -> None: ...
+
     @classmethod
     def deregister(cls, process: "FakeProcess") -> None: ...
+
     @classmethod
     def dispatch(
-        cls,
-        command: typing.Union[typing.Tuple[str, ...], str],
-        **kwargs: typing.Optional[typing.Dict]
+            cls,
+            command: typing.Union[typing.Tuple[str, ...], str],
+            **kwargs: typing.Optional[typing.Dict]
     ) -> FakePopen: ...
+
     @classmethod
     def _get_process(
-        cls, command: str
+            cls, command: str
     ) -> typing.Tuple[typing.Optional[typing.Deque[typing.Dict]], typing.Optional["FakeProcess"]]: ...
+
     @classmethod
     def allow_unregistered(cls, allow: bool) -> None: ...
+
     @classmethod
     def keep_last_process(cls, keep: bool) -> None: ...
+
 
 class IncorrectProcessDefinition(Exception): ...
 
+
 class FakeProcess:
     definitions: typing.DefaultDict[str, typing.Deque[typing.Union[typing.Dict, bool]]]
+
     def __init__(self) -> None: ...
+
     def register_subprocess(
-        self,
-        command: typing.Union[typing.List[str], typing.Tuple[str, ...], str],
-        stdout: OPTIONAL_TEXT_OR_ITERABLE = None,
-        stderr: OPTIONAL_TEXT_OR_ITERABLE = None,
-        returncode: int = 0,
-        wait: typing.Optional[float] = None,
-        callback: typing.Optional[typing.Callable] = None,
-        occurrences: int = 1,
-        stdin_callable: typing.Optional[typing.Callable] = None,
+            self,
+            command: typing.Union[typing.List[str], typing.Tuple[str, ...], str],
+            stdout: OPTIONAL_TEXT_OR_ITERABLE = None,
+            stderr: OPTIONAL_TEXT_OR_ITERABLE = None,
+            returncode: int = 0,
+            wait: typing.Optional[float] = None,
+            callback: typing.Optional[typing.Callable] = None,
+            occurrences: int = 1,
+            stdin_callable: typing.Optional[typing.Callable] = None,
     ) -> None: ...
-    def pass_command(self, command: typing.Union[typing.List[str], typing.Tuple[str, ...], str], occurrences: int=1) -> None: ...
+
+    def pass_command(self, command: typing.Union[typing.List[str], typing.Tuple[str, ...], str],
+                     occurrences: int = 1) -> None: ...
+
     def __enter__(self) -> "FakeProcess": ...
+
     def __exit__(self, *args: typing.List, **kwargs: typing.Dict) -> None: ...
+
     @classmethod
     def allow_unregistered(cls, allow: bool) -> None: ...
+
     @classmethod
     def keep_last_process(cls, keep: bool) -> None: ...
+
     @classmethod
     def context(cls) -> "FakeProcess": ...
