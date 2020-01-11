93e76a81b6f6b983598d9c39d0c903225d232825
Andrzej Klajnert <github@aklajnert.pl>
2020-01-11 13:43:04 +0100

    Update conf.py for docs

--- docs/conf.py
+++ docs/conf.py
@@ -52,6 +52,9 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 # a list of builtin themes.
 #
 html_theme = "alabaster"
+html_sidebars = {
+    "**": ["about.html", "navigation.html", "relations.html", "searchbox.html"]
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
