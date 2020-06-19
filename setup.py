00cfbf9e7f67ebe4b2d51542f830a1481c89d881
Andrzej Klajnert <github@aklajnert.pl>
2020-06-19 08:58:19 +0200

    Release new version.

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ if sys.version_info <= (3, 5):
 
 setup(
     name="pytest-subprocess",
-    version="0.1.4",
+    version="0.1.5",
     author="Andrzej Klajnert",
     author_email="python@aklajnert.pl",
     maintainer="Andrzej Klajnert",
