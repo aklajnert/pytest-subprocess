0944507a6c073c3e7de43c4c86eda8fa49c850ce
Andrzej Klajnert <github@aklajnert.pl>
2021-04-15 10:02:43 +0200

    Attempt to fix tests on Windows

--- noxfile.py
+++ noxfile.py
@@ -1,7 +1,7 @@
 import nox
 
 
-@nox.session(python=["3.6", "3.7", "3.8", "3.9", "pypy"])
+@nox.session(python=["3.6", "3.7", "3.8", "3.9", "pypy3"])
 def tests(session):
     session.install(".[test]")
     session.run("coverage", "run", "-m", "pytest", "-v")
