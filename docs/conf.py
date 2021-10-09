fcd347c9e2f3c1a47fb36d84bfdd2489061c17dc
Andrzej Klajnert <github@aklajnert.pl>
2021-10-09 09:58:45 +0200

    Change docs theme to furo

--- docs/conf.py
+++ docs/conf.py
@@ -59,7 +59,7 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "alabaster"
+html_theme = "furo"
 html_sidebars = {
     "**": ["about.html", "navigation.html", "relations.html", "searchbox.html"]
 }
