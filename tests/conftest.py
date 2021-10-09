f48945d12825514019e846ff903f338ddb4c49ba
Andrzej Klajnert <github@aklajnert.pl>
2021-10-09 11:41:20 +0200

    Move event_loop fixture to conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,4 @@
+import asyncio
 import faulthandler
 import os
 import sys
@@ -6,6 +7,20 @@ import pytest
 
 pytest_plugins = "pytester"
 faulthandler.enable(file=sys.stderr, all_threads=True)
+
+
+@pytest.fixture(autouse=True)
+def event_loop(request):
+    policy = asyncio.get_event_loop_policy()
+    if sys.platform == "win32":
+        if request.node.name.startswith("test_invalid_event_loop"):
+            loop = asyncio.SelectorEventLoop()
+        else:
+            loop = asyncio.ProactorEventLoop()
+    else:
+        loop = policy.get_event_loop()
+    yield loop
+    loop.close()
 
 
 @pytest.fixture(autouse=True)
