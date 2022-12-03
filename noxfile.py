e7c9e4c4c23f646edf3fb9cd679c9f766f136af7
Henry Schreiner <HenrySchreinerIII@gmail.com>
2022-12-03 04:33:32 -0500

    ci: update jobs and restore 3.6 (#108)
    Signed-off-by: Henry Schreiner <henryschreineriii@gmail.com>
    
    Signed-off-by: Henry Schreiner <henryschreineriii@gmail.com>

--- noxfile.py
+++ noxfile.py
@@ -1,7 +1,7 @@
 import nox
 
 
-@nox.session(python=["3.6", "3.7", "3.8", "3.9", "3.10", "3.11", "pypy3"])
+@nox.session(python=["3.6", "3.7", "3.8", "3.9", "3.10", "3.11", "pypy3.8"])
 def tests(session):
     session.install(".[test]")
     session.run("coverage", "run", "-m", "pytest", "-v", *session.posargs)
@@ -21,7 +21,8 @@ def mypy(session):
     session.run("mypy", "tests/test_typing.py", "--config-file=setup.cfg")
 
 
-@nox.session
+# Note sphinx-napoleon uses deprecated renames removed in 3.10
+@nox.session(python="3.9")
 def docs(session):
     session.install(".[docs]")
     session.run("sphinx-build", "-b", "html", "docs", "docs/_build", "-v", "-W")
