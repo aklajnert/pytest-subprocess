2fa0547cd30d41043c1defde051aaf6066faf1c1
Rasmus Pedersen <rasmus.pedersen@sky.uk>
2023-12-20 14:52:14 +0100

    fix: add missing Program type in ARGUMENT type union

--- pytest_subprocess/types.py
+++ pytest_subprocess/types.py
@@ -4,8 +4,7 @@ import os
 from typing import Sequence
 from typing import Union
 
-from .utils import Any
-from .utils import Command
+from .utils import Any, Command, Program
 
 OPTIONAL_TEXT = Union[str, bytes, None]
 OPTIONAL_TEXT_OR_ITERABLE = Union[
@@ -15,5 +14,5 @@ OPTIONAL_TEXT_OR_ITERABLE = Union[
     Sequence[Union[str, bytes]],
 ]
 BUFFER = Union[io.BytesIO, io.StringIO, asyncio.StreamReader]
-ARGUMENT = Union[str, Any, os.PathLike]
+ARGUMENT = Union[str, Any, os.PathLike, Program]
 COMMAND = Union[Sequence[ARGUMENT], str, Command]
